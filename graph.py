422a58ad027e9c99c6180b3bb535701265820d30
rsoender02 <93575263+rsoender02@users.noreply.github.com>
2021-11-03 01:23:55 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -9,8 +9,8 @@ p_t = [20, 22, 25, 18, 15, 15, 20, 19, 21, 12, 22, 25]
 r = 0.04
 add = max(p_t) * u_max + 1
 q_start = 5
-
 graph = {}
+
 graph[0, q_start] = {}
 
 
@@ -26,9 +26,7 @@ def determine_neighbors(e):
     return a, b
 
 
-for e in range(
-        determine_neighbors(q_start)[0],
-        determine_neighbors(q_start)[1] + 1):
+for e in range(determine_neighbors(q_start)[0], determine_neighbors(q_start)[1] + 1):
     graph[0, q_start][1, e] = exp(r * (-1 / T)) * (e - q_start) * p_t[0] + add
 
 for t in range(1, T):
@@ -36,10 +34,8 @@ for t in range(1, T):
         graph[(t, e)] = {}
         a, b = determine_neighbors(e)
         for i in range(a, b + 1):
-            graph[t, e][t + 1, i] = exp(r * (-(t + 1) / T)) * \
-                (i - e) * p_t[t] + add
+            graph[t, e][t + 1, i] = exp(r * (-(t + 1) / T)) * (i - e) * p_t[t] + add
 
 for e in range(0, 11):
     graph[(T, e)] = {}
-
 print(graph)
